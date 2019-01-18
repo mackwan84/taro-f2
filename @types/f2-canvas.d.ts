92407cadea2a3951a21a6213eb0fd36da2cac3b3
xin <i@xioxin.com>
2019-01-18 16:00:06 +0800

    add Example

--- @types/f2-canvas.d.ts
+++ @types/f2-canvas.d.ts
@@ -1,8 +1,7 @@
 import { ComponentClass } from 'react'
 
 export interface F2CanvasProps {
-  onInit: (canvas:any, width: number, height: number, F2:any) => any,
-  F2: any
+  onCanvasInit: (canvas:any, width: number, height: number) => any,
 }
 
 declare const F2Canvas: ComponentClass<F2CanvasProps>
